4ac28a0f818fcbc8a6f90dff53a25bb1f21ec4d1
Euthanazepam <euthanazepam@protonmail.com>
2024-07-12 21:58:05 +0000

    #update #cryptanalysis - hash message digest 5, removing .potfile

--- Cryptanalysis/Hash - Message Digest 5/get-flag.sh
+++ Cryptanalysis/Hash - Message Digest 5/get-flag.sh
@@ -2,8 +2,18 @@
 
 # https://www.root-me.org/en/Challenges/Cryptanalysis/Hash-Message-Digest-5
 
+DICTIONARY=rockyou.txt
+FILE=ch2.txt
+POTFILE=~/.local/share/hashcat/hashcat.potfile
+
+function removePotFile() {
+  if [ -f $POTFILE ]; then
+    rm $POTFILE
+  fi
+}
+
 function downloadAndExtractDictionary() {
-  if [ ! -f rockyou.txt ]; then
+  if [ ! -f $DICTIONARY ]; then
     wget https://raw.githubusercontent.com/zacheller/rockyou/master/rockyou.txt.tar.gz
     tar -xf rockyou.txt.tar.gz
 #    wget https://raw.githubusercontent.com/josuamarcelc/common-password-list/main/rockyou.txt/rockyou.txt.zip
@@ -12,15 +22,17 @@ function downloadAndExtractDictionary() {
 }
 
 function downloadTaskFile() {
-  if [ ! -f ch2.txt ]; then
+  if [ ! -f $FILE ]; then
     wget https://static.root-me.org/cryptanalyse/ch2/ch2.txt
   fi
 }
 
 function decryptHash() {
-  hashcat -m 0 -a 0 ch2.txt rockyou.txt
+  hashcat -m 0 -a 0 $FILE $DICTIONARY
+  cat $POTFILE | cut -d : -f 2
 }
 
+removePotFile
 downloadAndExtractDictionary
 downloadTaskFile
 decryptHash
