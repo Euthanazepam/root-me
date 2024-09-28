7fa722d80a4a636c6f0a614a42df4debe2d6ea08
Euthanazepam <euthanazepam@protonmail.com>
2024-09-28 12:03:52 +0000

    #add #cryptanalysis - DCC2

---
+++ Cryptanalysis/Hash/DCC2/get_flag.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# This script cracks the hash from the task https://www.root-me.org/en/Challenges/Cryptanalysis/Hash-DCC2
+
+DICTIONARY=rockyou.txt
+FILE=ch51.txt
+POTFILE=~/.local/share/hashcat/hashcat.potfile
+PACKAGE_NAME="hashcat"
+
+function checkHashcat()
+  # Check if hashcat is installed.
+  if [[ ! $(pacman -Ss $PACKAGE_NAME) ]]; then
+    echo -e "$PACKAGE_NAME isn't installed\nInstall it with 'pacman -Sy $PACKAGE_NAME'"
+    exit 1
+  fi
+
+function removePotFile() {
+  # Clears potfile with previously cracked passwords.
+  if [ -f $POTFILE ]; then
+    rm $POTFILE
+  fi
+}
+
+function downloadAndExtractDictionary() {
+  # Downloads the archive with the rockyou.txt dictionary and unpacks it.
+  if [ ! -f $DICTIONARY ]; then
+    wget https://raw.githubusercontent.com/zacheller/rockyou/master/rockyou.txt.tar.gz
+    tar -xf rockyou.txt.tar.gz
+#    wget https://raw.githubusercontent.com/josuamarcelc/common-password-list/main/rockyou.txt/rockyou.txt.zip
+#    unzip rockyou.txt.zip
+  fi
+}
+
+function downloadTaskFile() {
+  # Downloads the task file.
+  if [ ! -f $FILE ]; then
+    wget https://static.root-me.org/cryptanalyse/ch51/$FILE
+  fi
+}
+
+function decryptHash() {
+  # Cracks the hash from the task file by dictionary.
+  HASH=$(grep 'ROOTME.LOCAL/Administrator' $FILE | cut -d ':' -f 2)
+  hashcat -m 2100 -a 0 $HASH $DICTIONARY
+  cat $POTFILE | cut -d : -f 2
+}
+
+checkHashcat
+removePotFile
+downloadAndExtractDictionary
+downloadTaskFile
+decryptHash
